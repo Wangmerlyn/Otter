6e0d740c6496e6ea5181f5d2da83aa4520c270a3
Wangmerlyn <wsy0227@sjtu.edu.cn>
2023-09-03 23:16:27 +0800

    add git version checkout function

--- mimic-it/convert-it/make_instruction.py
+++ mimic-it/convert-it/make_instruction.py
@@ -2,6 +2,7 @@
 Module used to make an FunQA_instruction.json
 """
 import argparse
+import datetime
 from typing import Dict
 import json
 import os
@@ -23,10 +24,14 @@ def runCmd(command):
     return ret
 
 
-def get_git_version(path):
-    strs = os.path.split(file)
-    dir, fileName = strs[0], strs[1]
-    content = str(runCmd(cmd.format(dir, fileName)).stdout, "utf-8")
+def get_git_version(path: str = "."):
+    """
+    Change
+    """
+    cmd = 'cd {}; git log --pretty=format:"%h" -n 1'
+    content = str(runCmd(cmd.format(path)).stdout, "utf-8")
+    cmd = "cd -"
+    runCmd(cmd)
     # print(content)
     return content
 
@@ -50,48 +55,53 @@ def make_instruction(
     for pair in annotation:
         instruction_id = pair["ID"]
         instructions[f"FunQA_{instruction_id}"] = pair
-    for key in instructions.items():
-        instructions[key]["answer"] = instructions[key].pop("output")
+    for key, pair in instructions.items():
+        pair["answer"] = pair.pop("output")
 
     # search image keys for corresponding images
     prefix_list = ["_".join(i.split("_")[2:-1]) for i in keys_list]
 
-    for key in instructions.items():
-        video_name = instructions[key]["visual_input"].split(".")[0]
+    for key, pair in instructions.items():
+        video_name = pair["visual_input"].split(".")[0]
         start_index = bisect.bisect_left(prefix_list, video_name)
         end_index = bisect.bisect_right(prefix_list, video_name)
-        instructions[key]["image_ids"] = keys_list[start_index:end_index]
+        pair["image_ids"] = keys_list[start_index:end_index]
 
     # collecting rel_ins_ids
     # search for instructions with same video name
     rel_ins_ids_dict = {}
 
-    for key in instructions.items():
-        video = instructions[key]["visual_input"]
+    for key, pair in instructions.items():
+        video = pair["visual_input"]
         if video not in rel_ins_ids_dict:
             rel_ins_ids_dict[video] = []
         rel_ins_ids_dict[video].append(key)
 
     # add a rel_ins_ids attribute for each pair
-    for key in instructions.items():
-        video = instructions[key]["visual_input"]
+    for key, pair in instructions.items():
+        video = pair["visual_input"]
         list_copy = rel_ins_ids_dict[video][:]
         list_copy.remove(key)
-        instructions[key]["rel_ins_ids"] = list_copy
+        pair["rel_ins_ids"] = list_copy
 
     # remove unnecessary attributes
-    for key in instructions.items():
-        instructions[key].pop("visual_input")
-        instructions[key].pop("ID")
-        instructions[key].pop("task")
+    for key, pair in instructions.items():
+        pair.pop("visual_input")
+        pair.pop("ID")
+        pair.pop("task")
 
     # create a meta data dictionary
     meta_data = {}
-    meta_data["version"] = ""
+    meta_data["version"] = get_git_version()
+    meta_data["author"] = "NobleAI"
+    meta_data["time"] = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     output_json = {}
+    output_json["meta"] = meta_data
+    output_json["data"] = instructions
 
     with open(output_path, "w", encoding="utf-8") as output_file:
-        json.dump(instructions, output_file)
+        json.dump(output_json, output_file)
+    return output_json
 
 
 if __name__ == "__main__":
@@ -103,7 +113,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--annotation_path",
-        default="../../dataset/annotation_with_id/fun_qa_train.json",
+        default="../../dataset/annotation_with_ID/funqa_train.json",
         help="path to the annotation file.",
     )
     parser.add_argument(
@@ -116,5 +126,6 @@ if __name__ == "__main__":
     make_instruction_args: Dict[str, str] = {}
     make_instruction_args["image_json_path"] = args.image_json_path
     make_instruction_args["annotation_path"] = args.annotation_path
+    make_instruction_args["output_path"] = args.output_path
 
     make_instruction(**make_instruction_args)
